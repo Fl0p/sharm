eccfae79906e0d337fee91f41c8ad8c20ddcb793
Flop <flop@hackerspace.by>
2025-05-04 12:45:26 +0200

    🔧 Add Docker integration and environment configuration for SHARM music streaming
    - Introduced `.env` file to define and configure environment variables for ports used by MPD and YMPD.
    - Added a new `Dockerfile` to set up a Docker container for SHARM, replacing the previous separate MPD and YMPD containers with a unified configuration.
    - Updated `docker-compose.yml` to utilize the new Docker setup, with dynamic port mapping based on `.env` variables.
    - Modified `mpd.conf` to support environment-variable-based dynamic port configuration.
    - Refined `run.sh` to incorporate a more interactive CLI experience and handle start-up errors.
    - Created `start.sh` for initialization of services within the Docker container.

--- run.sh
+++ run.sh
@@ -4,13 +4,38 @@ cd $(dirname $0)
 
 pwd
 
+#load .env
+if [ -f .env ]; then
+    source .env
+fi
+
 docker compose down || true
 
-docker compose up -d
+docker compose up -d --build
 
-echo "Done"
+if [ $? -eq 0 ]; then
+    echo "Docker container started successfully"
+else
+    echo "Error starting Docker container"
+    exit 1
+fi
 
-echo "run 'mpv http://localhost:8000' to listen to the stream"
+echo "run 'mpv http://localhost:${MPD_STREAM_PORT}' to listen to the stream"
+
+echo "do you want to start listening to the stream? (y/n)"
+read answer
+
+if [ "$answer" = "y" ]; then
+    mpv http://localhost:${MPD_STREAM_PORT} \
+    --cache=no \
+    --demuxer-readahead-secs=0 \
+    --demuxer-max-bytes=8 \
+    --untimed \
+    --no-cache-pause \
+    --hr-seek=no \
+    --audio-buffer=0 \
+    --autosync=0
+fi
 
 exit 0
 
