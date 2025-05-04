b5a6961d22339b5c80c4db9b249250af23577431
Flop <flop@hackerspace.by>
2025-05-04 18:49:02 +0200

    🔧 Integrate Snapserver with MPD Streaming Setup
    This commit introduces Snapserver integration into the existing MPD streaming setup by making key configuration changes and updates across multiple files.
    
    - Updated `.env` file to hold new Snapserver environment variables for streamlined port and path configuration.
    - Modified `Dockerfile` to include Snapserver installation, configuration, and exposure of necessary ports for streaming and HTTP access.
    - Updated `docker-compose.yml` to define new arguments and corresponding port mappings for Snapserver.
    - Revised `mpd.conf` to include new audio output options such as ALSA and FIFO for enhanced streaming capabilities.
    - Configured `snapserver.conf` file to utilize dynamic FIFO paths and set up appropriate server and HTTP configurations.
    - Updated `start.sh` script to initialize snapserver and manage the FIFO pipeline for seamless data flow.
    
    These enhancements provide improved audio distribution functionality on the system.

--- start.sh
+++ start.sh
@@ -14,8 +14,14 @@ if [ -f .env ]; then
     source .env
 fi
 
+mkfifo ${FIFO_PATH}
+chmod 666 ${FIFO_PATH}
+
 echo "Starting mpd Client on port ${MPD_CLIENT_PORT} stream port ${MPD_STREAM_PORT}"
 mpd /etc/mpd.conf
+
+echo "Starting snapserver"
+# snapserver -c /etc/snapserver.conf
 
 echo "Starting ympd with port ${YMPD_PORT}"
 ympd -w ${YMPD_PORT}
