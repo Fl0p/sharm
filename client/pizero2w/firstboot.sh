3d211e6e6f8e6a0f5d3570b4036fa8d1e9a51525
Flop <flop@hackerspace.by>
2025-09-30 22:44:26 +0200

    🔧 Enable necessary hardware interfaces and set locale during first boot
    - Added configuration to enable I2C, I2S, and SPI interfaces in `/boot/firmware/config.txt`. It checks if the parameters are already present and adds them if missing, or uncomments them if commented out.
    - Configured system locale to `en_GB.UTF-8` to ensure correct language settings.
    - These changes help to ensure that the necessary peripherals are enabled and locale settings are properly configured on the first boot of the Raspberry Pi Zero 2 W.

--- client/pizero2w/firstboot.sh
+++ client/pizero2w/firstboot.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# raspi-config
+CONFIG="/boot/firmware/config.txt"
+for param in "dtparam=i2c_arm=on" "dtparam=i2s=on" "dtparam=spi=on"; do
+  if ! grep -q "^$param" "$CONFIG"; then
+    echo "$param" >> "$CONFIG"
+  else
+    sed -i "s/^#*$param/$param/" "$CONFIG"
+  fi
+done
+
+
 # --- base packages ---
 apt-get update
 apt-get -y upgrade
@@ -8,6 +19,10 @@ apt-get -y install \
   git wget cmake curl vim htop i2c-tools pigpio \
   python3 python3-pip python3-venv python3-setuptools python3-smbus python3-pigpio\
   alsa-utils pulseaudio pulseaudio-utils libasound2 mpv 
+
+#locale
+sudo locale-gen en_GB.UTF-8
+sudo update-locale LANG=en_GB.UTF-8 LC_ALL=en_GB.UTF-8
 
 # edit gpio service
 # sudo systemctl edit pigpiod
