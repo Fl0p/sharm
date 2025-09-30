bf78a8f017573fb9f718b63ac84dcfa8eac401fb
Flop <flop@hackerspace.by>
2025-09-30 23:13:25 +0200

    🔧 Enable I2C, I2S, and SPI, and configure locale for Raspberry Pi Zero 2 W setup
    This commit enhances the first boot script for the Raspberry Pi Zero 2 W by enabling I2C, I2S, and SPI interfaces, and configuring locale settings to en_GB.UTF-8 to avoid warnings during package installations. It updates the system with critical package installations using non-interactive mode to handle configuration changes. Additionally, it modifies the pigpiod service to ensure proper GPIO service management and installs the WM8960 Audio HAT, ensuring a clean setup by removing temporary files after installation. A reboot is triggered to apply these changes effectively.

--- client/pizero2w/firstboot.sh
+++ client/pizero2w/firstboot.sh
@@ -8,6 +8,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # raspi-config
+echo "Enabling i2c, i2s, spi"
 CONFIG="/boot/firmware/config.txt"
 for param in "dtparam=i2c_arm=on" "dtparam=i2s=on" "dtparam=spi=on"; do
   if ! grep -q "^$param" "$CONFIG"; then
@@ -17,29 +18,48 @@ for param in "dtparam=i2c_arm=on" "dtparam=i2s=on" "dtparam=spi=on"; do
   fi
 done
 
-
-# --- base packages ---
-apt-get update
-apt-get -y upgrade
-apt-get -y install \
-  git wget cmake curl vim htop i2c-tools pigpio \
-  python3 python3-pip python3-venv python3-setuptools python3-smbus python3-pigpio\
-  alsa-utils pulseaudio pulseaudio-utils libasound2 mpv 
-
-#locale
+# locale setup first to avoid warnings during package installation
+echo "Updating locale"
+sed -i 's/^# *en_GB.UTF-8/en_GB.UTF-8/' /etc/locale.gen
 locale-gen en_GB.UTF-8
 update-locale LANG=en_GB.UTF-8 LC_ALL=en_GB.UTF-8
+export LANG=en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
+
+echo "Updating system"
+# --- base packages ---
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
+apt-get -y -o Dpkg::Options::="--force-confnew" install \
+  git wget cmake curl vim htop i2c-tools pigpio \
+  python3 python3-pip python3-venv python3-setuptools python3-smbus python3-pigpio\
+  alsa-utils pulseaudio pulseaudio-utils libasound2 mpv
 
 # edit gpio service
 # sudo systemctl edit pigpiod
-# sudo cat /etc/systemd/system/pigpiod.service
-# sudo systemctl daemon-reload
-# sudo systemctl enable --now pigpiod
+mkdir -p /etc/systemd/system/pigpiod.service.d
+sudo tee /etc/systemd/system/pigpiod.service.d/override.conf > /dev/null <<'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/pigpiod -t 0 -s 10 -x 0x08C00010
+EOF
+systemctl daemon-reload
+systemctl restart pigpiod
+systemctl enable --now pigpiod
+#check gpio
+raspi-gpio get 23
+
 
 # --- WM8960-Audio-HAT ---
+echo "Installing WM8960-Audio-HAT"
+CURRENT_DIR=$(pwd)
+cd /tmp
 git clone https://github.com/Fl0p/WM8960-Audio-HAT.git
 cd WM8960-Audio-HAT
 ./install.sh
+cd "$CURRENT_DIR"
+rm -rf /tmp/WM8960-Audio-HAT
 reboot
 
 # --- test ---
